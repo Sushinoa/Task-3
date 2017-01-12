8d7d48a99c5ca3af4824191a245edbbd6fbb0d87
Сушинская Оксана (Back-end dev) <sushinskaya.89@mail.ru>
2017-01-12 10:17:30 +0200

    Task-3

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+
+// jQuery(document).ready(function($) {
+//     $("[data-tooltip]").mousemove(function (eventObject) {
+//         $data_tooltip = $(this).attr("data-tooltip");
+//         $(".tool_tip").text($data_tooltip)
+//             .css({
+//                 "top" : eventObject.pageY + 5,
+//                 "left" : eventObject.pageX + 5
+//             })
+//             .show();
+//     }).mouseout(function () {
+//         $(".tool_tip").hide()
+//             .text("")
+//             .css({
+//                 "top" : 0,
+//                 "left" : 0
+//             });
+//     });
+// });
---
+++ src/js/test.js
@@ -0,0 +1,8 @@
+
+(function() {
+    var tree = 'oak';
+
+    var funcOne = function () {};
+
+    console.log( tree );
+})();
