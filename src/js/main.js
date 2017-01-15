53cedb72d3d49ae13853cf0d20aca032a46bacaf
Сушинская Оксана (Back-end dev) <sushinskaya.89@mail.ru>
2017-01-15 17:59:37 +0200

    tool_tip

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,21 @@
 jQuery(document).ready(function ($) {
-    $('.show-hint').on("click", function (e) {
-        $('.tool_tip').fadeIn(500);
 
-
-    //     e = e || window.event;
-    //     e.preventDefault();
-    //     var ypos = $(this).offset().top + 24;
-    //     var xpos = $(this).offset().left;
-    //     var RealHint = $(this).data('hint');
-    //     $(RealHint).css('top', ypos);
-    //     $(RealHint).css('left', xpos);
-    //     $(RealHint).toggle('fast');
-    //     return;
-    // });
-    //
-    // $('.prm-cross').on('click', function () {
-    //     $(this).parent().hide('fast');
-    //     return false;
-    // });
-    //
-    // document.onclick = function (e) {
-    //     if ($(e.target).hasClass('real-hint') == false)
-    //         $('.real-hint').hide('fast');
-       // return;
-    });
-    $('.show-hint2').on("click", function (e) {
+    $('.show-hint_img').on("click", function (e) {
         e.preventDefault();
-        // var ypos = $(this).position();
-        // var xpos = $(this).position();
-        var ypos = $(this).offset();
-        var xpos = $(this).offset();
-        // var RealHint = $(this).data('hint');
-        // $(RealHint).css('top', ypos);
-        // $(RealHint).css('left', xpos);
-        // $(RealHint).toggle('fast');
+        var tool_tip = $('.tool_tip');
+        var ypos = $(this).offset().top;
+        var xpos = $(this).offset().left;
+        tool_tip.css({"top": ypos, "left": xpos - 100});
+        tool_tip.fadeIn(500);
 
-        $('.tool_tip').fadeIn(500);
-
+    });
+    $('.show-hint_name').on("click", function (e) {
+        e.preventDefault();
+        var tool_tip = $('.tool_tip');
+        var ypos = $(this).offset().top;
+        var xpos = $(this).offset().left;
+        tool_tip.css({"top": ypos-5, "left": xpos - 180});
+        tool_tip.fadeIn(500);
 
     });
     $('.tool_tip').on('click',function (e) {
