1b42f718101f109b2bffb235564ffc78bdd86c75
Сушинская Оксана (Back-end dev) <sushinskaya.89@mail.ru>
2017-01-15 02:18:53 +0200

    tooltip

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,35 @@
 jQuery(document).ready(function ($) {
-    $('.real-show-hint').on("click", function (e) {
-        e = e || window.event;
-        e.preventDefault();
-        var ypos = $(this).offset().top + 24;
-        var xpos = $(this).offset().left;
-        var RealHint = $(this).data('hint');
-        $(RealHint).css('top', ypos);
-        $(RealHint).css('left', xpos);
-        $(RealHint).toggle('fast');
-        return;
-    });
+    $('.show-hint').on("click", function (e) {
+        $('.tool_tip').fadeIn(300);
 
-    $('.prm-cross').on('click', function () {
-        $(this).parent().hide('fast');
-        return false;
+    //     e = e || window.event;
+    //     e.preventDefault();
+    //     var ypos = $(this).offset().top + 24;
+    //     var xpos = $(this).offset().left;
+    //     var RealHint = $(this).data('hint');
+    //     $(RealHint).css('top', ypos);
+    //     $(RealHint).css('left', xpos);
+    //     $(RealHint).toggle('fast');
+    //     return;
+    // });
+    //
+    // $('.prm-cross').on('click', function () {
+    //     $(this).parent().hide('fast');
+    //     return false;
+    // });
+    //
+    // document.onclick = function (e) {
+    //     if ($(e.target).hasClass('real-hint') == false)
+    //         $('.real-hint').hide('fast');
+       // return;
     });
+    $('.tool_tip').on('click',function (e) {
+        $('.tool_tip').fadeOut(300);
 
-    document.onclick = function (e) {
-        if ($(e.target).hasClass('real-hint') == false)
-            $('.real-hint').hide('fast');
-        return;
-    }
+    });
+    // document.onclick = function (e) {
+    //     if ($(e.target).hasClass('.content') == false)
+    //         $('.tool_tip').fadeOut(300);
+    //
+    // };
 });
